4a2942dc4919122d4288c3efdaaa8f5d96df89fe
Zoe Nguyen <nganguyen0703@gmail.com>
2023-02-08 23:27:42 +0800

    fix: update api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8800/api/v1';
+const BASE_URL = 'https://hello-world-sg.cyclic.app/api/v1';
 
 export default axios.create({
   baseURL: BASE_URL
