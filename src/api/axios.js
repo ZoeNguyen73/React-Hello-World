1ad22685d937c995bf3495af6e8ab9370a76bb9b
Zoe Nguyen <nganguyen0703@gmail.com>
2025-11-13 15:18:01 +0800

    feat: update server url in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://express-hello-world-juxe.onrender.com/api/v1';
+const BASE_URL = 'https://express-hello-world-1-6zlx.onrender.com/api/v1';
 
 export default axios.create({
   baseURL: BASE_URL
